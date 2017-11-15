5ba29cce1281b0aac6fafe1b4815a1fabf3f306e
Etay Sade <sade.etay@gmail.com>
2017-11-15 09:22:41 +0200

    test changes

--- functions.php
+++ functions.php
@@ -2,8 +2,16 @@
 
 // enqueue the child theme stylesheet
 
-Function wp_schools_enqueue_scripts() {
-wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-wp_enqueue_style( 'childstyle' );
+function wp_schools_enqueue_scripts() {
+  wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+  wp_enqueue_style( 'childstyle' );
 }
+
+function custom_pon_scripts() {
+  wp_register_style( 'custom_pon', get_stylesheet_directory_uri() . '/custom_pon.css'  );
+  wp_enqueue_style( 'custom_pon' );
+}
+
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+add_action( 'wp_enqueue_scripts', 'custom_pon_scripts', 11);
